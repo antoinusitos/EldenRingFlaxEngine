8e3c32eb44a85904826012165e3f8ce8d71ee076
Antoine Gargasson <antoine.gargasson@starbreeze.com>
2023-11-16 16:19:20 +0100

    Added Camera movement

--- Source/Game/Character/Player/PlayerCamera.cs
+++ Source/Game/Character/Player/PlayerCamera.cs
@@ -1,4 +1,5 @@
 ﻿using FlaxEngine;
+using System;
 using System.Transactions;
 
 namespace Game
@@ -8,10 +9,19 @@ namespace Game
         public static PlayerCamera instance = null;
         public PlayerManager player = null;
         public Camera cameraObject = null;
+        public Actor cameraPivotTransform;
 
         [Header("Camera Settings")]
-        private Vector3 cameraVelocity = Vector3.Zero;
         private float cameraSmoothSpeed = 1.0f;
+        private float leftAndRightRotationSpeed = 220.0f;
+        private float upAndDownRotationSpeed = 220.0f;
+        private float minimumPivot = -30.0f;
+        private float maximumPicot = 60.0f;
+
+        [Header("Camera Values")]
+        private Vector3 cameraVelocity = Vector3.Zero;
+        private float leftAndRightLookAngle = 0.0f;
+        private float upAndDownLookAngle = 0.0f;
 
         public override void OnStart()
         {
@@ -32,6 +42,7 @@ namespace Game
             if(player != null)
             {
                 HandleFollowTarget();
+                HandleRotations();
             }
         }
 
@@ -39,6 +50,27 @@ namespace Game
         {
             Vector3 targetCameraPosition = Vector3.SmoothDamp(Transform.Translation, player.Transform.Translation, ref cameraVelocity, cameraSmoothSpeed * Time.DeltaTime);
             Actor.Position = targetCameraPosition;
+        }
+
+        private void HandleRotations()
+        {
+            leftAndRightLookAngle += (PlayerInputManager.instance.cameraHorizontalInput * leftAndRightRotationSpeed) * Time.DeltaTime;
+            upAndDownLookAngle -= (PlayerInputManager.instance.cameraVerticalInput * upAndDownRotationSpeed) * Time.DeltaTime;
+            upAndDownLookAngle = Mathf.Clamp(upAndDownLookAngle, minimumPivot, maximumPicot);
+
+            Vector3 cameraRotation = Vector3.Zero;
+            Quaternion targetRotation = Quaternion.Identity;
+
+            cameraRotation.Y = leftAndRightLookAngle;
+            targetRotation = Quaternion.Euler(cameraRotation);
+            Actor.Orientation = targetRotation;
+
+            cameraRotation = Vector3.Zero;
+            cameraRotation.X = upAndDownLookAngle;
+            targetRotation = Quaternion.Euler(cameraRotation);
+            Transform t = cameraPivotTransform.LocalTransform;
+            t.Orientation = targetRotation;
+            cameraPivotTransform.LocalTransform = t;
         }
     }
 }
--- Source/Game/Character/Player/PlayerInputManager.cs
+++ Source/Game/Character/Player/PlayerInputManager.cs
@@ -9,10 +9,16 @@ namespace Game
 
         private bool forceFirstUpdate = true;
 
+        [Header("Movement Input")]
         private Vector2 movementInput = Vector2.Zero;
         public float verticalInput = 0f;
         public float horizontalInput = 0f;
         public float moveAmount = 0f;
+
+        [Header("Camera Input")]
+        private Vector2 cameraInput = Vector2.Zero;
+        public float cameraVerticalInput = 0f;
+        public float cameraHorizontalInput = 0f;
 
         public override void OnStart()
         {
@@ -62,7 +68,14 @@ namespace Game
         {
             base.OnUpdate();
 
+            movementInput.X = Input.GetAxis("Horizontal");
+            movementInput.Y = Input.GetAxis("Vertical");
+
+            cameraInput.X = Input.GetAxis("CameraHorizontal");
+            cameraInput.Y = Input.GetAxis("CameraVertical");
+
             HandleMovementInput();
+            HandleCameraMovementInput();
         }
 
         public override void OnDestroy()
@@ -72,8 +85,8 @@ namespace Game
 
         private void HandleMovementInput()
         {
-            verticalInput = Input.GetAxis("Vertical");
-            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = movementInput.Y;
+            horizontalInput = movementInput.X;
 
             moveAmount = Mathf.Clamp(Mathf.Abs(verticalInput) + Mathf.Abs(horizontalInput), 0, 1);
 
@@ -85,6 +98,12 @@ namespace Game
             {
                 moveAmount = 1;
             }
+        }
+
+        private void HandleCameraMovementInput()
+        {           
+            cameraVerticalInput = cameraInput.Y;
+            cameraHorizontalInput = cameraInput.X;
         }
     }
 }
